2422bc63c2b9954a69a0cfd65d6ee65dbeb662ec
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-07-20 13:00:29 +0300

    This commit will add the confirm.html file

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 function matchPassword() {  
     var pw1 = document.getElementById("password1");  
     var pw2 = document.getElementById("password2");  
-    if(pw1 != pw2)  
+    if(pw1 !== pw2)  
     {   
       alert("Passwords did not match");  
     } else {  
