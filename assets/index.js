5e39d44b35e9fc6be6f5e1d0530b0d99e2ad0f58
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-07-20 11:22:55 +0300

    This commit will add the onclick submit function in js and html files

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+function matchPassword() {  
+    var pw1 = document.getElementById("password1");  
+    var pw2 = document.getElementById("password2");  
+    if(pw1 != pw2)  
+    {   
+      alert("Passwords did not match");  
+    } else {  
+      alert("Password created successfully");  
+    }  
+  }  
